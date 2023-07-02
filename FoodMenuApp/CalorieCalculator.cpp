9434b7cb6c1b83dd50653af18ef26b711c305cba
Haralambi Manolov <h.manolov77@gmail.com>
2023-07-02 22:02:05 +0300

    Additional

---
+++ FoodMenuApp/CalorieCalculator.cpp
@@ -0,0 +1,31 @@
+#include "inc/CalorieCalculator.hpp"
+
+CaloriesData calculate_calories(CaloriesData data, char gender, int age, double height,
+                       double weight, int activity)
+{
+    if (toupper(gender) == 'M') {
+        data.bmr_calories = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+        data.weight_maintain_calories = determine_maintain_calories(data, activity);
+        data.weight_gain_calories = data.weight_maintain_calories + 300;
+        data.weight_lose_calories = data.weight_maintain_calories - 300;
+    }
+    else if (toupper(gender) == 'F') {
+        data.bmr_calories = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+        data.weight_maintain_calories = determine_maintain_calories(data, activity);
+        data.weight_gain_calories = data.weight_maintain_calories + 250;
+        data.weight_lose_calories = data.weight_maintain_calories - 250;
+    }
+    return data;
+}
+
+int determine_maintain_calories(const CaloriesData &data, const int &activity) {
+    /* Calculate the total calories required for maintaining body weight.
+       More activity - more calories. */
+    switch (activity) {
+        case 1: return (data.bmr_calories * 1.2);
+        case 2: return (data.bmr_calories * 1.375);
+        case 3: return (data.bmr_calories * 1.55);
+        case 4: return (data.bmr_calories * 1.725);
+    }
+    return 0;
+}
--- FoodMenuApp/inc/CalorieCalculator.hpp
+++ FoodMenuApp/inc/CalorieCalculator.hpp
@@ -3,20 +3,18 @@
 #include <cctype>
 #include <ostream>
 
-namespace Calculator {
-    struct CaloriesData {
-        int bmr_calories;
-        int weight_maintain_calories;
-        int weight_gain_calories;
-        int weight_lose_calories;
-    };
+struct CaloriesData {
+    int bmr_calories;
+    int weight_maintain_calories;
+    int weight_gain_calories;
+    int weight_lose_calories;
+};
 
-    CaloriesData calculate_calories(
-            CaloriesData data, char gender, int age,
-            double height, double weight, int activity
-    );
+CaloriesData calculate_calories(
+        CaloriesData data, char gender, int age,
+        double height, double weight, int activity
+);
 
-    int determine_maintain_calories(const CaloriesData &data, const int &activity);
-}
+int determine_maintain_calories(const CaloriesData &data, const int &activity);
 
 #endif // _CALORIE_CALCULATOR_HPP
