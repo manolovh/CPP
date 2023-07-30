c3beceae5da65797b18389401a55853bbe5b459a
Harry <h.manolov77@gmail.com>
2023-07-30 16:37:46 +0300

    problem 3

--- LeetCode/Medium/AddTwoNums.cpp
+++ LeetCode/Medium/AddTwoNums.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include <vector>
 
 struct ListNode
@@ -15,116 +16,58 @@ class Solution
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
     {
-        ListNode* list;
-        return list;
+        ListNode* temp_node = new ListNode();
+        ListNode* first_node = temp_node;
+        ListNode* current_node = first_node;
+
+        delete temp_node;
+        int extra_point = 0;
+
+        while (l1 != nullptr || l2 != nullptr || extra_point)
+        {
+            int total_sum = 0;
+
+            if (l1 != nullptr)
+            {
+                total_sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr)
+            {
+                total_sum += l2->val;
+                l2 = l2->next;
+            }
+
+            total_sum += extra_point;
+            extra_point = total_sum / 10;
+
+            ListNode* node_ptr = new ListNode(total_sum % 10);
+            current_node->next = node_ptr;
+            current_node = current_node->next;
+        }
+        return first_node;
     }
 };
 
 int main()
 {
-    ListNode* l1 = new ListNode(9);
-    ListNode* l2 = new ListNode(9, l1);
-    ListNode* l3 = new ListNode(9, l2);
+    ListNode* l1 = new ListNode(3);
+    ListNode* l2 = new ListNode(4, l1);
+    ListNode* l3 = new ListNode(2, l2);
 
-    ListNode* h1 = new ListNode(9);
-    ListNode* h2 = new ListNode(9, h1);
-    ListNode* h3 = new ListNode(9, h2);
+    ListNode* h1 = new ListNode(4);
+    ListNode* h2 = new ListNode(6, h1);
+    ListNode* h3 = new ListNode(5, h2);
 
-    std::vector<int> l_vector;
-    std::vector<int> h_vector;
-    std::vector<int> final_vector;
 
-    int total_nodes_l = 0;
-    while (true)
+    Solution sol;
+    ListNode* p = sol.addTwoNumbers(l3, h3);
+
+    while (p)
     {
-        total_nodes_l++;
-
-        if (!l3->next)
-        {
-            l_vector.push_back(l3->val);
-            break;
-        }
-        else
-        {
-            l_vector.push_back(l3->val);
-            l3 = l3->next;
-        }
+        std::cout << p->val;
+        p = p->next;
     }
 
-    int total_nodes_h = 0;
-    while (true)
-    {
-        total_nodes_h++;
-
-        if (!h3->next)
-        {
-            h_vector.push_back(h3->val);
-            break;
-        }
-        else
-        {
-            h_vector.push_back(h3->val);
-            h3 = h3->next;
-        }
-    }
-
-    std::cout << "\nTotal l: " << total_nodes_l;
-    std::cout << "\nTotal h: " << total_nodes_h;
-
-    // ListNode* final_list = new ListNode();
-    int temp_value;
-    bool has_one_plus;
-
-    if (total_nodes_h == total_nodes_l)
-    {
-        for (int i=0; i < total_nodes_l; i++)
-        {
-            temp_value = l_vector[i] + h_vector[i];
-            if (temp_value > 9)
-            {
-                has_one_plus = true;
-                final_vector.push_back(temp_value - 10);
-            }
-            else if (has_one_plus)
-            {
-                temp_value++;
-                has_one_plus = false;
-                if (temp_value > 9)
-                {
-                    has_one_plus = true;
-                    final_vector.push_back(temp_value - 10);
-
-                }
-                else
-                {
-                    final_vector.push_back(temp_value);
-                }
-            }
-            else
-            {
-                final_vector.push_back(temp_value);
-            }
-
-            if (i == (total_nodes_l - 1) && has_one_plus)
-                final_vector.push_back(1);
-        }
-    }
-    else if (total_nodes_h < total_nodes_l)
-    {
-        for (int i=0; i < total_nodes_h; i++)
-        {
-
-        }
-    }
-    else if (total_nodes_h > total_nodes_l)
-    {
-        for (int i=0; i < total_nodes_h; i++)
-        {
-            
-        }
-    }
     std::cout << std::endl;
-
-    for (auto elem: final_vector)
-        std::cout << elem;
 }
