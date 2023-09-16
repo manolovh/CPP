16fced1f645d08645e3ea5530ffc051234184b06
Harry <h.manolov77@gmail.com>
2023-09-16 13:55:25 +0300

    continue

--- LeetCode/Hard/MaxGoodPeople.cpp
+++ LeetCode/Hard/MaxGoodPeople.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <numeric>
+#include <algorithm>
 
 using std::vector;
 
@@ -9,44 +10,58 @@ class Solution {
 public:
     int maximumGood(vector<vector<int>>& statements)
     {
-        int max_good_people = 0;
-        int current_max = 0;
         size_t size = statements[0].size();
+        int max_good_people_1;
+        int max_good_people_2;
+        vector<int> all_scores;
 
-        vector<vector<int>> people(size, vector<int>(size, 2));
-
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < size; i ++)
         {
+            // Person `i` is a good person and tells the truth
+            max_good_people_1 = size;
             for (int j = 0; j < size; j++)
             {
                 if (statements[i][j] == 0)
                 {
-                    people[i][j] = statements[i][j];
+                    max_good_people_1--;
+                }
+                else if (statements[i][j] == 2)
+                {
+                    int k = 0;
+                    while (k < size)
+                    {
+                        if (statements[k][j] == 0)
+                        {
+                            max_good_people_1--;
+                            break;
+                        }
+                        else if (statements[k][i] == 0 && statements[k][j] == 1)
+                        {
+                            max_good_people_1--;
+                            break;
+                        }
+                        else
+                        {
+                            k++;
+                        }
+                    }
                 }
             }
+
+            // Person `i` is a bad person and lies
+            max_good_people_2 = size - 1;
+            for (int j = 0; j < size; j++)
+            {
+                if (statements[i][j] == 1)
+                {
+                    max_good_people_2--;
+                }
+            }
+
+            all_scores.push_back((max_good_people_1 > max_good_people_2) ? max_good_people_1 : max_good_people_2);
         }
 
-
-        for (int i = 0; i < size; i++)
-        {
-            if (people[0][i] == 1)
-                current_max += 1;
-
-            if (current_max > max_good_people)
-                max_good_people = current_max;
-        }
-
-        current_max = 0;
-        for (int i = 0; i < size; i++)
-        {
-            if (people[0][i] == 0)
-                current_max += 1;
-
-            if (current_max > max_good_people)
-                max_good_people = current_max;
-        }
-
-        return max_good_people;
+        return *(std::max_element(all_scores.begin(), all_scores.end()));
     }
 };
 
