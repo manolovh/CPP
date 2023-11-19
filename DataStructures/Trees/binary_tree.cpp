4d392dd0116c319965be6ca6a69823bee984aef8
Harry <h.manolov77@gmail.com>
2023-11-19 14:55:49 +0200

    iterative pre/in/post-orders

--- DataStructures/Trees/binary_tree.cpp
+++ DataStructures/Trees/binary_tree.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <utility>
 
 struct Node
 {
@@ -53,8 +54,88 @@ void display(Node* root)
     }
 }
 
+void PreorderIteration(Node* root)
+{
+    std::vector<Node*> stack;
+    std::cout << "Preorder = ";
+    while (root || !stack.empty())
+    {
+        if (root)
+        {
+            std::cout << root->val << " ";
+            stack.push_back(root);
+            root = root->left;
+        }
+        else if (!stack.empty())
+        {
+            root = stack[stack.size() - 1];
+            stack.pop_back();
+            root = root->right;
+        }
+    }
+}
+
+void InorderIteration(Node* root)
+{
+    std::vector<Node*> stack;
+    std::cout << "Inorder = ";
+    while (root || !stack.empty())
+    {
+        if (root)
+        {
+            stack.push_back(root);
+            root = root->left;
+        }
+        else if (!stack.empty())
+        {
+            root = stack[stack.size() - 1];
+            stack.pop_back();
+            std::cout << root->val << " ";
+            root = root->right;
+        }
+    }
+}
+
+void PostorderIteration(Node* root)
+{
+    std::vector<std::pair<Node*, bool>> stack;
+    std::cout << "Postorder = ";
+    std::pair<Node*, bool> p = std::make_pair(root, false);
+    while (p.first || !stack.empty())
+    {
+        if (p.first)
+        {
+            stack.push_back(p);
+            p = std::make_pair(p.first->left, false);
+        }
+        else if (!stack.empty())
+        {
+            p = stack[stack.size() - 1];
+            stack.pop_back();
+            if (!p.second)
+            {
+                p = std::make_pair(p.first, true);
+                stack.push_back(p);
+                p = std::make_pair(p.first->right, false);
+            } else
+            {
+                std::cout << p.first->val << " ";
+                p = std::make_pair(nullptr, false);
+            }
+        }
+    }
+}
+
 int main()
 {
     Node* node = create();
-    display(node);
+
+    InorderIteration(node);
+    std::cout << std::endl;
+
+    PreorderIteration(node);
+    std::cout << std::endl;
+    
+    PostorderIteration(node);
+    std::cout << std::endl;
 }
